73b98009bd911d365d61313696a28d6a65a88dcc
Pallavi <pallavink53@gmail.com>
2025-07-03 20:52:10 +0200

    Debug stmt

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -11,6 +11,12 @@ def spark():
     )
 
 def test_raw_tables_exist(spark):
+    ##Debugging only
+    catalogs = [row.catalog for row in spark.sql("SHOW CATALOGS").collect()]
+    print(f"Available catalogs: {catalogs}")
+    current_catalog = spark.sql("SELECT current_catalog()").collect()[0][0]
+    print(f"Current catalog: {current_catalog}")
+
     spark.sql("USE CATALOG assignment")
     tables = spark.sql("SHOW TABLES IN raw").select("tableName").rdd.flatMap(lambda x: x).collect()
     for expected in ["airports", "countries", "runways"]:
