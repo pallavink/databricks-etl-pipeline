d7cf0b51620eb7ded8b20ea20af4984aa172ec5c
Pallavi <pallavink53@gmail.com>
2025-07-04 12:01:07 +0200

    Final Commit 4

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -7,7 +7,7 @@ import os
 @pytest.fixture(scope="session")
 def databricks_client():
     conn = sql.connect(
-        server_hostname = os.environ["DATABRICKS_SERVER_HOSTNAME"],
+        server_hostname = os.environ["DATABRICKS_HOST"],
         http_path = os.environ["DATABRICKS_HTTP_PATH"],
         access_token = os.environ["DATABRICKS_TOKEN"]
     )
